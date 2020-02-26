933fed44cd7dc0d9167b87277e10620274e1c805
asadlib11 <asadalibaloch059@gmail.com>
2020-02-26 16:30:52 +0500

    Showing the basic modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(public matDialog: MatDialog) { }
+
   title = 'angular-reusable-dialog';
+  
+  openModal() {
+    const dialogConfig = new MatDialogConfig();
+    // The user can't close the dialog by clicking outside its body
+    dialogConfig.disableClose = false;
+    dialogConfig.id = "modal-component"; //id to use in global style.css
+    dialogConfig.height = "350px";
+    dialogConfig.width = "600px";
+
+    //opening modal component in the modal
+    const modalDialog = this.matDialog.open(ModalComponent, dialogConfig);
+
+  }
 }
