da0a91b8bd9a3f3aea8bd834e037d583bc9049bd
asadlib11 <asadalibaloch059@gmail.com>
2020-02-26 17:15:32 +0500

    modal with injectible data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,41 @@ export class AppComponent {
 
   title = 'angular-reusable-dialog';
   
-  openModal() {
+  openLogoutModal() {
+    const userId = "user01";
+    const dialogConfig = new MatDialogConfig();
+    // The user can't close the dialog by clicking outside its body
+    dialogConfig.disableClose = true;
+    dialogConfig.id = "modal-component";
+    dialogConfig.height = "350px";
+    dialogConfig.width = "600px";
+    dialogConfig.data = {
+      name: "logout",
+      title: "Are you sure you want to logout?",
+      description: "Logging out will result in you being unauthorized and unable to view contents of profile",
+      actionButtonText: "Logout",
+      userId: userId
+    }
+    const modalDialog = this.matDialog.open(ModalComponent, dialogConfig);
+  }
+
+  openDeleteProductModal(){
+    const productId = "prod01";
     const dialogConfig = new MatDialogConfig();
     // The user can't close the dialog by clicking outside its body
     dialogConfig.disableClose = false;
     dialogConfig.id = "modal-component"; //id to use in global style.css
     dialogConfig.height = "350px";
     dialogConfig.width = "600px";
-
+    dialogConfig.data = {
+      name: "deleteProduct",
+      title: "Are you sure you want to delete this product?",
+      description: "If you continue, the product with ID " + productId + " will be deleted.",
+      actionButtonText: "Delete",
+      productId: productId
+    }
     //opening modal component in the modal
     const modalDialog = this.matDialog.open(ModalComponent, dialogConfig);
-
   }
+
 }
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public modalData: any,
+  ) { 
+    console.log(this.modalData);
+  }
 
-  ngOnInit(): void {
+  ngOnInit() { }
+
+  actionFunction() {
+    this.closeModal();
+  }
+
+  closeModal() {
+    this.dialogRef.close();
   }
 
 }
